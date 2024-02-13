8a4b7d9b5a6b8e6ded14484b3e45476d675d2295
haruto17 <s1902272@sendai-nct.jp>
2024-02-14 00:55:43 +0900

    とりあえずoxを置くところまで

--- src/main.zig
+++ src/main.zig
@@ -1,15 +1,66 @@
 const std = @import("std");
+const io = std.io;
+
+fn draw_board(board: [3][3][]const u8) !void {
+    const stdout = io.getStdOut().writer();
+
+    try stdout.print("  1 2 3\n", .{});
+
+    for (board, 0..) |value, i| {
+        try stdout.print("{} {s} {s} {s}\n", .{ i + 1, value[0], value[1], value[2] });
+    }
+}
+
+fn nextToken(reader: anytype, buffer: []u8) []const u8 {
+    return reader.readUntilDelimiter(
+        buffer,
+        ' ',
+    ) catch unreachable;
+}
+
+fn nextLine(reader: anytype, buffer: []u8) []const u8 {
+    const line = reader.readUntilDelimiterOrEof(
+        buffer,
+        '\n',
+    ) catch unreachable;
+
+    if (@import("builtin").os.tag == .windows) {
+        return std.mem.trimRight(u8, line.?, "\r");
+    } else {
+        return line.?;
+    }
+}
+
+fn parseUsize(buf: []const u8) usize {
+    return std.fmt.parseInt(usize, buf, 10) catch unreachable;
+}
 
 pub fn main() !void {
-    // const matrix: [3][3]u32 = .{
-    //     .{ 1, 2, 3 },
-    //     .{ 4, 5, 6 },
-    //     .{ 7, 8, 9 },
-    // };
+    var board: [3][3][]const u8 = .{
+        .{ "-", "-", "-" },
+        .{ "-", "-", "-" },
+        .{ "-", "-", "-" },
+    };
 
-    var count: usize = 1;
+    const stdout = io.getStdOut().writer();
+    const stdin = io.getStdIn();
 
-    while (count <= 9) : (count += 1) {
-        std.debug.print("{d}\n", .{count});
+    try stdout.print("Tick Tack Toe\n", .{});
+
+    var line_buf: [20]u8 = undefined;
+    var i: u8 = 1;
+    while (i <= 9) {
+        try draw_board(board);
+
+        const x = parseUsize(nextToken(stdin.reader(), &line_buf));
+        const y = parseUsize(nextLine(stdin.reader(), &line_buf));
+
+        if (i % 2 != 0) {
+            board[x - 1][y - 1] = "o";
+        } else if (i % 2 == 0) {
+            board[x - 1][y - 1] = "x";
+        }
+
+        i += 1;
     }
 }
